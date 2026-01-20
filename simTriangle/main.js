ceac35199ae6f7b59d57130212a768cba82fe6bc
Bryan Lopez <brainalexis119@gmail.com>
2026-01-20 12:33:01 -0700

    TRIANGLE DRAWN

--- simTriangle/main.js
+++ simTriangle/main.js
@@ -1,27 +1,27 @@
 
 const vertexShaderText = `
     precision mediump float;
-    
+
     attribute vec2 vertPosition;
-    attribute vec3 verColor;
+    attribute vec3 vertColor;
     varying vec3 fragColor;
 
     void main(){
         fragColor = vertColor;
-        gl_Position = vec4(vertposition,0.0,1.0) 
+        gl_Position = vec4(vertPosition,0.0,1.0); 
     }
 `; //gl posiition is where you want the triangel vec4(x,y,z,A)
 
 const fragmentShaderText = 
 `
     precision mediump float;
-    varying vec 3 fragColor;
+    varying vec3 fragColor;
 
     void main(){
-        gl_fragColor = vec4(fragColor,1.0); 
+        gl_FragColor = vec4(fragColor,1.0); 
     }
-`
-main()
+`;
+main();
 async function main(){
     //all this is to set up the background of where you want to draw your 
     //project the "canvas"
@@ -48,7 +48,31 @@ async function main(){
     const triangelVertexBufferObject = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, triangelVertexBufferObject);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangelVertices),gl.STATIC_DRAW);
-    
+
+    const posiitionAttribLocation = gl.getAttribLocation(shaderprogram,'vertPosition')
+    gl.vertexAttribPointer(
+        posiitionAttribLocation,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0,
+    )
+    gl.enableVertexAttribArray(posiitionAttribLocation)
+
+    const colorAttributeLoacation = gl.getAttribLocation(shaderprogram,'vertColor')
+    gl.vertexAttribPointer(
+        colorAttributeLoacation,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT
+    )
+
+    gl.enableVertexAttribArray(colorAttributeLoacation)
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
 }
 
 function initShaderProgram(gl,vsSource,fsSource){ //vertex and fragment shader sources at top
@@ -58,23 +82,25 @@ function initShaderProgram(gl,vsSource,fsSource){ //vertex and fragment shader s
     //combine both the shaders into a shaderprogram
 
     const shaderProgram = gl.createProgram();
-    gl.attatchShader(shaderProgram,vertexShader);
-    gl.attatchShader(shaderProgram,fragmentShader);
+    gl.attachShader(shaderProgram,vertexShader);
+    gl.attachShader(shaderProgram,fragmentShader);
 
-    if (!gl.getProgramParameter(shader,gl.LINK_STATUS)){
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram,gl.LINK_STATUS)){
         alert(
             `Unable to initialize program ${gl.getProgramInfo()}`
         );
         return null;
     }
     gl.validateProgram(shaderProgram);
-    if (!gl.getProgramParameter(shader,gl.VALIDATE_STATUS)){
+    if (!gl.getProgramParameter(shaderProgram,gl.VALIDATE_STATUS)){
         return;
     }
 
     gl.useProgram(shaderProgram);
     return shaderProgram
-}
+};
 
 
 function loadShader(gl,type,source){
@@ -82,7 +108,7 @@ function loadShader(gl,type,source){
     const shader = gl.createShader(type);
 
     //grab both of the shader object and send it over
-    gl.shaderSource(shader,source)
+    gl.shaderSource(shader,source);
     //compile the shader program
     gl.compileShader(shader);
 
