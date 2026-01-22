6c17e47a7f7b2467be77ad03426c7781622b3477
Bryan Lopez <brainalexis119@gmail.com>
2026-01-21 19:06:47 -0700

    working on balls

---
+++ fallinbalss/main.js
@@ -0,0 +1,133 @@
+const vertexShader = `
+    precision mediump float;
+
+    attribute vec2 vertPosition;
+    uniform mat4 uProjectionMatrix;
+    void main(){
+        gl_Position = uProjectionMatrix * vec4(vertPosition, 0.0, 1.0);
+    };
+`
+const fragmentShader =`
+    precision mediump float;
+
+    uniform vec4 uColor;
+    
+    void main(){
+        gl_FragColor = uColor;
+    };
+`
+async function main() {
+    
+    console.log("connection good")
+    //get the canvas from the html
+    const canvas = document.getElementById("webcanvas");
+    //connect to webgl library
+    const gl = canvas.getContext('webgl');
+    //check that it is good
+    if (!gl){
+        alert("your browser doesn't support html5")
+    };
+    //clear the color and its buffer bit
+    gl.clearColor(0.5,0.7,0.2,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    //init the shader program
+    //go make initshaderProgram
+    let shaderProgram = initShaderProgram(gl,vertexShader,fragmentShader)
+
+    //create function to find circle vertices positions
+
+    function createCircleVerticies(sides){
+        const vertices = []
+        vertices.push(0)
+        vertices.push(0)
+        for(let i=0;i < sides+1;i++){
+            const radians = i/sides *2*Math.PI
+            x = Math.cos(radians)
+            y = Math.sin(radians)
+            vertices.push(x)
+            vertices.push(y)
+        }
+        return vertices
+    };
+
+    const sides = 64;
+    const circlVertices = createCircleVerticies(sides);
+    //create buffer for verticesu
+    const circleBufferObject = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,circleBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(circlVertices),gl.STATIC_DRAW);
+
+    //vertex attributes
+
+    const positionAttributes = gl.getAttribLocation(shaderProgram,"vertPosition");
+    gl.vertexAttribPoint(
+        positionAttributes,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        2 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    )
+    const uniformColorLocation = gl.getAttribLocation(shaderProgram,"uColor")
+    const circColor = [0.2,0.5,0.2,1.0]
+    gl.uniform4v(
+        uniformColorLocation,
+        circColor
+    );
+
+    const ProjectionMatrixUniformLocation = gl.getUniformLocation(shaderProgram,"uProjectionMatrix")
+    
+};
+
+function initShaderProgram(gl,vsSource,fsSource){
+    //get the source of the shaders and send them to get compiled
+    const vertexShader = loadshader(gl,gl.VERTEX_SHADER,vsSource)
+    const fragmentShader = loadshader(gl,gl.FRAGMENT_SHADER,fsSource)
+
+    //create the actual program 
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram,vertexShader);
+    gl.attachShader(shaderProgram,fragmentShader);
+    gl.linkProgram(shaderProgram);
+    
+    //test if the two shader attached
+    if(!gl.getProgramParameters(shaderProgram,gl.LINK_STATUS)){
+        alert(
+            `unable to initialize shader program ${gl.getShaderInfoLog(shaderProgram)}`
+        )
+        return null
+    };
+
+    //test if program works
+    gl.validateProgram(shaderProgram)
+    if(!gl.getProgramParameters(shaderProgram,gl.VALIDATE_STATUS)){
+        console.error("ERROR validating program",gl.getProgramInfoLog(shader));
+        return
+    };
+    gl.useProgram(shaderProgram);
+
+    return shaderProgram;
+};
+
+function loadshader(gl,type,source){
+    //create specific shader
+    const shader = gl.createShader(type)
+
+    //connect the shader type to the source code
+    gl.shaderSource(shader,source);
+
+    //compile shader
+    gl.compileShader(shader);
+
+    if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS)){
+        alert(
+            `an error happened when compiling shader ${gl.getShaderInfoLog(shader)}`
+        );
+        gl.deleteShader(shader)
+        return null;
+    };
+    return shader;
+
+}
