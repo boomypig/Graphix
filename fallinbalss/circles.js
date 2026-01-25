ee8e8a83d73808ba35c27f720aebfc29351a9fcb
Bryan Lopez <brainalexis119@gmail.com>
2026-01-24 17:35:28 -0700

    THEY MOVE

--- fallinbalss/circles.js
+++ fallinbalss/circles.js
@@ -1,5 +1,112 @@
-class Circle{
-    constructor(){
-        this.color  
+class Circle {
+  constructor(xhigh, xlow, yhigh, ylow) {
+    this.color = [Math.random(), Math.random(), Math.random(), 1];
+    this.sides = 64;
+    this.xhigh = xhigh;
+    this.xlow = xlow;
+    this.yhigh = yhigh;
+    this.ylow = ylow;
+    // HERE WE SHOULD BE ABLE TO MAKE A RANDOM SIZE CIRCLE IN RANDOM SPOT
+    this.size = 1.0 + Math.random();
+    const minx = xlow + this.size;
+    const maxx = xhigh - this.size;
+    this.x = minx + Math.random() * (maxx - minx);
+    const miny = ylow + this.size;
+    const maxy = yhigh - this.size;
+    this.y = miny + Math.random() * (maxy - miny);
+
+    this.dx = Math.random() * 2 + 2;
+    this.dy = Math.random() * 2 + 2;
+    if (Math.random() < 0.5) {
+      this.dy = -this.dy;
     }
+    if (Math.random() < 0.5) {
+      this.dx = -this.dx;
+    }
+  }
+  update(DT) {
+    if (this.x + this.dx * DT + this.size > this.xhigh) {
+      this.dx = -Math.abs(this.dx);
+    }
+    if (this.x + this.dx * DT - this.size < this.xlow) {
+      this.dx = Math.abs(this.dx);
+    }
+    if (this.y + this.dy * DT + this.size > this.yhigh) {
+      this.dy = -Math.abs(this.dy);
+    }
+    if (this.y + this.dy * DT - this.size < this.ylow) {
+      this.dy = Math.abs(this.dy);
+    }
+
+    // THIS IS THE VELOCITY CHANGING THE POSITION
+    this.x += this.dx * DT;
+    this.y += this.dy * DT;
+  }
+  draw(gl, shaderProgram) {
+    drawCircle(
+      gl,
+      shaderProgram,
+      this.sides,
+      this.color,
+      this.size,
+      this.x,
+      this.y,
+    );
+  }
 }
+
+function drawCircle(gl, shaderProgram, sides, color, size, x, y) {
+  const circleVertices = createCircleVertices(sides);
+  const circleBufferObject = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, circleBufferObject);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(circleVertices),
+    gl.STATIC_DRAW,
+  );
+
+  const positionAttribute = gl.getAttribLocation(shaderProgram, "vertPosition");
+
+  gl.vertexAttribPointer(
+    positionAttribute, //where the attribute is
+    2, // num of elements
+    gl.FLOAT, //floats sending
+    gl.FALSE,
+    2 * Float32Array.BYTES_PER_ELEMENT, //size of vertex
+    0, //off set of where this data starts
+  );
+  gl.enableVertexAttribArray(positionAttribute);
+
+  const uniformColorLocation = gl.getUniformLocation(shaderProgram, "uColor");
+
+  gl.uniform4fv(uniformColorLocation, color);
+
+  const modelViewMatrixUniformLocation = gl.getUniformLocation(
+    shaderProgram,
+    "uModelViewMatrix",
+  );
+
+  const modelViewMatrix = mat4.create();
+  mat4.translate(modelViewMatrix, modelViewMatrix, [x, y, 0]);
+  mat4.scale(modelViewMatrix, modelViewMatrix, [size, size, 1]);
+  gl.uniformMatrix4fv(modelViewMatrixUniformLocation, false, modelViewMatrix);
+
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, sides + 2);
+}
+
+function createCircleVertices(sides) {
+  const vertices = [];
+  vertices.push(0);
+  vertices.push(0);
+  for (let i = 0; i < sides + 1; i++) {
+    const radians = (i / sides) * 2 * Math.PI;
+    const x = Math.cos(radians);
+    const y = Math.sin(radians);
+    vertices.push(x);
+    vertices.push(y);
+  }
+  return vertices;
+}
+
+export { Circle, drawCircle };
--- fallinbalss/main.js
+++ fallinbalss/main.js
@@ -1,21 +1,25 @@
-const vertexShader = `
-    precision mediump float;
+//MOVED TO RESPECTive FRAGMENT AND VERTEX TXT FILES
+// const vertexShader = `
+//     precision mediump float;
 
-    attribute vec2 vertPosition;
-    uniform mat4 uProjectionMatrix;
-    void main(){
-        gl_Position = uProjectionMatrix * vec4(vertPosition, 0.0, 1.0);
-    }
-`;
-const fragmentShader = `
-    precision mediump float;
+//     attribute vec2 vertPosition;
+//     uniform mat4 uProjectionMatrix;
+//     void main(){
+//         gl_Position = uProjectionMatrix * vec4(vertPosition, 0.0, 1.0);
+//     }
+// `;
+// const fragmentShader = `
+//     precision mediump float;
 
-    uniform vec4 uColor;
-    
-    void main(){
-        gl_FragColor = uColor;
-    }
-`;
+//     uniform vec4 uColor;
+
+//     void main(){
+//         gl_FragColor = uColor;
+//     }
+// `;
+
+import { Circle } from "./circles.js";
+import { initShaderProgram } from "./shader.js";
 main();
 async function main() {
   console.log("connection good");
@@ -33,59 +37,64 @@ async function main() {
 
   //init the shader program
   //go make initshaderProgram
+  let vertexShader = await (await fetch("simple.vs")).text();
+  let fragmentShader = await (await fetch("simple.fs")).text();
   let shaderProgram = initShaderProgram(gl, vertexShader, fragmentShader);
 
-  //create function to find circle vertices positions
+  gl.useProgram(shaderProgram);
 
-  function createCircleVerticies(sides) {
-    const vertices = [];
-    vertices.push(0);
-    vertices.push(0);
-    for (let i = 0; i < sides + 1; i++) {
-      const radians = (i / sides) * 2 * Math.PI;
-      const x = Math.cos(radians);
-      const y = Math.sin(radians);
-      vertices.push(x);
-      vertices.push(y);
-    }
-    return vertices;
-  }
+  //   //create function to find circle vertices positions
+  // MOVED TO CIRCLE.js
+  //   function createCircleVerticies(sides) {
+  //     const vertices = [];
+  //     vertices.push(0);
+  //     vertices.push(0);
+  //     for (let i = 0; i < sides + 1; i++) {
+  //       const radians = (i / sides) * 2 * Math.PI;
+  //       const x = Math.cos(radians);
+  //       const y = Math.sin(radians);
+  //       vertices.push(x);
+  //       vertices.push(y);
+  //     }
+  //     return vertices;
+  //   }
 
-  const sides = 64;
-  const circlVertices = createCircleVerticies(sides);
-  //create buffer for verticesu
-  const circleBufferObject = gl.createBuffer();
+  //   const sides = 64;
+  //   const circlVertices = createCircleVerticies(sides);
+  //   //create buffer for verticesu
+  //   const circleBufferObject = gl.createBuffer();
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, circleBufferObject);
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array(circlVertices),
-    gl.STATIC_DRAW,
-  );
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, circleBufferObject);
+  //   gl.bufferData(
+  //     gl.ARRAY_BUFFER,
+  //     new Float32Array(circlVertices),
+  //     gl.STATIC_DRAW,
+  //   );
 
-  //vertex attributes
+  //   //vertex attributes
 
-  const positionAttributes = gl.getAttribLocation(
-    shaderProgram,
-    "vertPosition",
-  );
-  gl.vertexAttribPointer(
-    positionAttributes,
-    2,
-    gl.FLOAT,
-    gl.FALSE,
-    2 * Float32Array.BYTES_PER_ELEMENT,
-    0,
-  );
-  gl.enableVertexAttribArray(positionAttributes);
-  const uniformColorLocation = gl.getUniformLocation(shaderProgram, "uColor");
-  const circColor = [1, 0, 1, 1];
-  gl.uniform4fv(uniformColorLocation, circColor);
+  //   const positionAttributes = gl.getAttribLocation(
+  //     shaderProgram,
+  //     "vertPosition",
+  //   );
+  //   gl.vertexAttribPointer(
+  //     positionAttributes,
+  //     2,
+  //     gl.FLOAT,
+  //     gl.FALSE,
+  //     2 * Float32Array.BYTES_PER_ELEMENT,
+  //     0,
+  //   );
+  //   gl.enableVertexAttribArray(positionAttributes);
+  //   const uniformColorLocation = gl.getUniformLocation(shaderProgram, "uColor");
+  //   const circColor = [1, 0, 1, 1];
+  //   gl.uniform4fv(uniformColorLocation, circColor);
 
   const ProjectionMatrixUniformLocation = gl.getUniformLocation(
     shaderProgram,
     "uProjectionMatrix",
   );
+
   const aspect = canvas.clientWidth / canvas.clientHeight;
   const projectionMatrix = mat4.create();
   const yhigh = 10;
@@ -95,55 +104,84 @@ async function main() {
   mat4.ortho(projectionMatrix, xlow, xhigh, ylow, yhigh, -1, 1);
   gl.uniformMatrix4fv(ProjectionMatrixUniformLocation, false, projectionMatrix);
 
-  gl.drawArrays(gl.TRIANGLE_FAN, 0, sides + 2);
-}
+  const numCircles = 8;
 
-function initShaderProgram(gl, vsSource, fsSource) {
-  //get the source of the shaders and send them to get compiled
-  const vertexShader = loadshader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadshader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  //create the actual program
-  const shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  //test if the two shader attached
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert(
-      `unable to initialize shader program ${gl.getShaderInfoLog(shaderProgram)}`,
-    );
-    return null;
+  const circleArray = [];
+  for (let i = 0; i < numCircles; i++) {
+    const c = new Circle(xhigh, xlow, yhigh, ylow);
+    circleArray.push(c);
   }
 
-  //test if program works
-  gl.validateProgram(shaderProgram);
-  if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
-    console.error("ERROR validating program", gl.getProgramInfoLog(shader));
-    return;
-  }
-  gl.useProgram(shaderProgram);
+  let previousTime = 0;
+  function redraw(currentTime) {
+    currentTime *= 0.001;
+    let DT = currentTime - previousTime;
+    previousTime = currentTime;
+    if (DT < 0.1) {
+      DT = 0.1;
+    }
 
-  return shaderProgram;
+    for (let i = 0; i < numCircles; i++) {
+      circleArray[i].update(DT);
+    }
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    for (let i = 0; i < numCircles; i++) {
+      circleArray[i].draw(gl, shaderProgram);
+    }
+
+    requestAnimationFrame(redraw);
+  }
+
+  requestAnimationFrame(redraw);
 }
 
-function loadshader(gl, type, source) {
-  //create specific shader
-  const shader = gl.createShader(type);
+// //THIS HAS BEEN MOVED TO SHADER.JS
+// function initShaderProgram(gl, vsSource, fsSource) {
+//   //get the source of the shaders and send them to get compiled
+//   const vertexShader = loadshader(gl, gl.VERTEX_SHADER, vsSource);
+//   const fragmentShader = loadshader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-  //connect the shader type to the source code
-  gl.shaderSource(shader, source);
+//   //create the actual program
+//   const shaderProgram = gl.createProgram();
+//   gl.attachShader(shaderProgram, vertexShader);
+//   gl.attachShader(shaderProgram, fragmentShader);
+//   gl.linkProgram(shaderProgram);
 
-  //compile shader
-  gl.compileShader(shader);
+//   //test if the two shader attached
+//   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+//     alert(
+//       `unable to initialize shader program ${gl.getShaderInfoLog(shaderProgram)}`,
+//     );
+//     return null;
+//   }
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(
-      `an error happened when compiling shader ${gl.getShaderInfoLog(shader)}`,
-    );
-    gl.deleteShader(shader);
-    return null;
-  }
-  return shader;
-}
+//   //test if program works
+//   gl.validateProgram(shaderProgram);
+//   if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
+//     console.error("ERROR validating program", gl.getProgramInfoLog(shaderProgram));
+//     return;
+//   }
+//   gl.useProgram(shaderProgram);
+
+//   return shaderProgram;
+// }
+
+// function loadshader(gl, type, source) {
+//   //create specific shader
+//   const shader = gl.createShader(type);
+
+//   //connect the shader type to the source code
+//   gl.shaderSource(shader, source);
+
+//   //compile shader
+//   gl.compileShader(shader);
+
+//   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+//     alert(
+//       `an error happened when compiling shader ${gl.getShaderInfoLog(shader)}`,
+//     );
+//     gl.deleteShader(shader);
+//     return null;
+//   }
+//   return shader;
+// }
